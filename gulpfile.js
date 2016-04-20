657d2489e99fb1d9722537cb0edad19db51a3c9c
Adam K Dean <adamkdean@googlemail.com>
2016-04-20 21:03:05 +0100

    Added initial boilerplate with babel + eslint

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const del = require('del');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const lint = require('gulp-eslint');
+const notice = require('gulp-notice');
+
+gulp.task('lint', () =>
+  gulp.src([
+    '**/*.js',
+    '!**/*.min.js',
+    '!dist/**',
+    '!node_modules/**'
+  ])
+  .pipe(lint())
+  .pipe(lint.format())
+  .pipe(lint.failAfterError())
+);
+
+gulp.task('clean', () => {
+  del([
+    'dist/**/*',
+    '!dist/',
+    '!dist/.gitkeep'
+  ]);
+});
+
+gulp.task('transpile', ['clean'], () =>
+  gulp.src([
+    'src/**/*.js'
+  ])
+  .pipe(babel())
+  .pipe(notice())
+  .pipe(gulp.dest('dist'))
+);
+
+gulp.task('default', ['transpile']);
